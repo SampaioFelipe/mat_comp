bd5aa9cb6d552c222f088e444774f4abf5afa06e
Romão Martines <roh.martines@gmail.com>
2018-06-27 20:37:51 -0300

    Adicao do algoritmo de dijkstra

--- prim/prim.py
+++ prim/prim.py
@@ -43,6 +43,7 @@ def plotGraph(G):
 def prim(G, start):
     '''
     Function that receives a graph and a starting node, and returns a minimal spanning tree
+    using Prim's approach
     '''
     total_weight = 0
     V = G.number_of_nodes()
@@ -65,7 +66,43 @@ def prim(G, start):
         for v in range(V):
             if (u, v) in G.edges():
                 if (mst_set[v] == False) and (G[u][v]['weight'] < dist[v]):
-                    dist[v] = G[u][v]['weight'] # dijsktra -> G[u][v]['weight'] + dist[u]
+                    dist[v] = G[u][v]['weight']
+                    parent[v] = u   
+
+    print("Total weight: ", total_weight)
+
+    for X in range(V):
+        if parent[X] != -1:
+            if (parent[X], X) in G.edges():
+                nx.draw_networkx_edges(G, pos, edgelist = [(parent[X], X)], width = 2.5, alpha = 0.6, edge_color = '#00FF00')
+
+def dijkstra(G, start):
+    '''
+    Function that receives a graph and a starting node, and returns a minimal spanning tree
+    using the Dijkstra's approach
+    '''
+    total_weight = 0
+    V = G.number_of_nodes()
+    dist = []
+    parent = [None] * V
+    mst_set = []
+
+    for _ in range(V):
+        dist.append(math.inf)
+        mst_set.append(False)
+
+    dist[0] = 0
+    parent[0] = -1
+
+    for _ in range(0, V):
+        u = minDistance(dist, mst_set, V)
+        mst_set[u] = True
+        total_weight = total_weight + dist[u]
+
+        for v in range(V):
+            if (u, v) in G.edges():
+                if (mst_set[v] == False) and (G[u][v]['weight'] < dist[v]):
+                    dist[v] = G[u][v]['weight'] + dist[u]
                     parent[v] = u   
 
     print("Total weight: ", total_weight)
@@ -77,15 +114,19 @@ def prim(G, start):
 
 
 if __name__ == "__main__":
-    A = np.loadtxt(sys.argv[1])
+    A = np.loadtxt(sys.argv[2])
 
     try:
         G = nx.from_numpy_matrix(A)
     except:
-        G = nx.read_weighted_edgelist(sys.argv[1])
+        G = nx.read_weighted_edgelist(sys.argv[2])
         G = nx.convert_node_labels_to_integers(G)
 
-    pos = plotGraph(G)
-    prim(G, pos)
+    if (sys.argv[1] == "-p"):
+        pos = plotGraph(G)
+        prim(G, pos)
+    elif (sys.argv[1] == "-d"):
+        pos = plotGraph(G)
+        dijkstra(G, pos)
     
     plt.show()
